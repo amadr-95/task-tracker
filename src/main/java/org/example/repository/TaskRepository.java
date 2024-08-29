f892c4419c9ef5be8cd022c554a11be2e4f1c71b
Amador <amasabcar@outlook.com>
2024-08-29 16:47:31 +0200

    clean up imports

--- src/main/java/org/example/repository/TaskRepository.java
+++ src/main/java/org/example/repository/TaskRepository.java
@@ -3,7 +3,6 @@ package org.example.repository;
 import org.example.model.Task;
 
 import java.util.List;
-import java.util.UUID;
 
 public interface TaskRepository {
     Task createTask(Task task);
